52a8b94f705b833dfaf0a95ed7d21595f24e3b2d
Kiran Joshi <mkjoshi30@gmail.com>
2017-05-02 03:47:22 +0530

    Developed and pushed the app on github.

---
+++ scripts/directives/todos.js
@@ -0,0 +1,8 @@
+angular.module('mainApp')
+    .directive('todos', function(){
+        return{
+          templateUrl :'templates/todos.html',
+            controller: 'mainCtrl',
+            replace : true
+        }
+    })
